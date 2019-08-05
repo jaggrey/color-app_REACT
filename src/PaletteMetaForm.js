682125ef21e2c9fca811ecfb116834c8a6ca4919
Jeffrey <jeffreyaggrey@gmail.com>
2019-08-05 07:57:03 +0000

    make PaletteList responsive

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -57,7 +56,7 @@ class PaletteMetaForm extends Component {
   }
 
   render() {
-    const { handleSubmit, hideForm } = this.props;
+    const { hideForm } = this.props;
     const { stage, newPaletteName } = this.state;
 
     return (
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
   root: {
     backgroundColor: "blue",
@@ -11,7 +13,13 @@ export default {
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-start",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "70%"
+    },
+    [sizes.down("xs")]: {
+      width: "70%"
+    }
   },
   nav: {
     display: "flex",
@@ -32,6 +40,13 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)"
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "1fr",
+      gridGap: "1.5rem"
+    }
   }
 }
