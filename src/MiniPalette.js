4b71cc4eddda3b19808dd736481cc4ef1f5d23fe
Jeffrey <jeffreyaggrey@gmail.com>
2019-08-07 11:37:13 +0000

    add Delete Confirmation Dialog

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -11,7 +11,7 @@ class MiniPalette extends Component {
 
   deletePalette(e) {
     e.stopPropagation();
-    this.props.handleDelete(this.props.id);
+    this.props.openDialog(this.props.id);
     // pass in an id
     // delete it from palettes
     // set LS with new palettes
